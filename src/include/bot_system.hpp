194e3650762470c71f9b277d468c7fcc3dcdebb9
tagciccone <tagciccone@gmail.com>
2025-11-10 18:50:39 -0500

    and a little more setup for the hpp & launch files

--- src/include/bot_system.hpp
+++ src/include/bot_system.hpp
@@ -1,6 +1,15 @@
 #ifndef BOT_SYSTEM_HPP
 #define BOT_SYSTEM_HPP
 
-// TODO
+#include "hardware_interface/system_interface.hpp"
+
+namespace rrc_igvc_bot
+{
+
+class BotSystemHardware : public hardware_interface::SystemInterface {
+    public:
+
+    private:
+}
 
 #endif //BOT_SYSTEM_HPP
