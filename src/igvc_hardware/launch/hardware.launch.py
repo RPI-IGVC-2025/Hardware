0e29ca329da623dcba8961638ba58c80e66474e1
tagciccone <tagciccone@gmail.com>
2026-02-03 22:04:05 +0000

    Update hardware bringups to have the proper ODrive drivetrain

--- src/igvc_hardware/launch/hardware.launch.py
+++ src/igvc_hardware/launch/hardware.launch.py
@@ -1,19 +1,43 @@
 from launch import LaunchDescription
+from launch.substitutions import Command, FindExecutable, PathJoinSubstitution, LaunchConfiguration
+from launch_ros.substitutions import FindPackageShare
+from launch_ros.actions import Node
 
 def generate_launch_description():
     # Declare args
     declared_arguments = []
-
-    #TODO add use_mock_hardware to declared arguments?
     
-    
-    # Init args
-
-    # Get URDF
-
     # Get nodes
+    robot_controllers = PathJoinSubstitution(
+        [
+            FindPackageShare("hardware"),
+            "config",
+            "bot_controllers.yaml",
+        ]
+    )
     
-    # TODO In the examples, the controller manager is not spawned until the joint state broadcaster is. Do we need to do this?
-    nodes = []
+    control_node = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        parameters=[
+            # TODO in the ODrive botwheel explorer example, the description contents are also passed in here.
+            robot_controllers
+        ],
+        output="both",
+    )
+    
+    robot_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["drivetrain_controller", "--controller-manager", "/controller_manager"],
+    )
+
+
+    # TODO In the examples, the controller manager is not spawned until the joint state broadcaster is finished spawning. Implement if we have problems regarding that.
+    
+    nodes = [
+        control_node,
+        robot_controller_spawner
+    ]
 
     return LaunchDescription(declared_arguments + nodes)
