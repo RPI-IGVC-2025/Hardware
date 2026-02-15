0da9be590b1f8f709ef8e1477df9391a14d6199a
tagciccone <tagciccone@gmail.com>
2026-02-15 18:09:22 +0000

    partially working drive simulation

--- src/igvc_description/launch/publisher.launch.py
+++ src/igvc_description/launch/publisher.launch.py
@@ -92,8 +92,8 @@ def generate_launch_description():
   launch_description.add_action(declare_use_sim_time_cmd)
 
   # Add any actions
-  launch_description.add_action(start_joint_state_publisher_cmd)
-  launch_description.add_action(start_joint_state_publisher_gui_node)
+  # launch_description.add_action(start_joint_state_publisher_cmd)
+  # launch_description.add_action(start_joint_state_publisher_gui_node)
   launch_description.add_action(start_robot_state_publisher_cmd)
 
   return launch_description
--- src/igvc_hardware/launch/control.launch.py
+++ src/igvc_hardware/launch/control.launch.py
@@ -4,6 +4,8 @@ from launch_ros.substitutions import FindPackageShare
 from launch_ros.actions import Node
 
 from launch.substitutions import PathJoinSubstitution
+from launch.event_handlers import OnProcessExit
+from launch.actions import DeclareLaunchArgument, RegisterEventHandler
 
 def generate_launch_description():
     # Declare args
@@ -33,10 +35,30 @@ def generate_launch_description():
         executable="spawner",
         arguments=["bot_drive_controller",
                    "--controller-manager", "/controller_manager"],
+        remappings=[('~/cmd_vel','/cmd_vel')]
+
+    )
+    
+    
+    joint_state_broadcaster_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster", "--controller-manager", "/controller_manager"],
     )
 
+    
+    delay_robot_controller_spawner_after_joint_state_broadcaster_spawner = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=joint_state_broadcaster_spawner,
+            on_exit=[robot_controller_spawner],
+        )
+    )
+
+
+
     nodes = [
-        control_node,
+        # control_node,
+        joint_state_broadcaster_spawner,
         robot_controller_spawner
     ]
 
