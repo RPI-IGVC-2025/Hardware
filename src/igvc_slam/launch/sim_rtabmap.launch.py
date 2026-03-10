d784b38ad864d938520434a5572494fc6e51766e
EnzoRap117 <enzorap117@gmail.com>
2026-03-10 22:50:27 +0000

    Ran into some troubles getting rviz to display properly.
    - Reorganized rtab launch arguments by category
    
    - Added the following arguments: 'icp_odometry', 'visual_odometry', 'subscribe_scan', 'scan_topic'

--- src/igvc_slam/launch/sim_rtabmap.launch.py
+++ src/igvc_slam/launch/sim_rtabmap.launch.py
@@ -21,32 +21,45 @@ def generate_launch_description():
         PythonLaunchDescriptionSource(rtabmap_launch_path),
         launch_arguments={
             'args' : '--delete_db_on_start',
+            # Topics
             'depth_topic' : '/camera/camera/depth/image_rect_raw',
             'rgb_topic' : '/camera/camera/color/image_raw',
             'camera_info_topic' : '/camera/camera/depth/camera_info',
-            'approx_sync' : 'true',
-            'frame_id' : 'base_footprint',
-            'log_level' : 'debug', 
-            'publish_tf_odom' : 'true',
-            'odom_topic' : '/odom',
-            'odom_frame_id' : 'odom',
-            'sync_queue_size' : '10', #TODO verify
-            'topic_queue_size' : '10', #TODO verify
-            'rgbd_sync' : 'true',
-            'approx_rgbd_sync' : 'true',
-            'subscribe_depth' : 'false',
-            'subscribe_rgbd' : 'true',
-            'visual_odometry' : 'true',
-            'approx_sync_max_interval' : '0.01',
-            'qos' : '1',
-            'rviz' : 'true',
-            'use_sim_time' : 'true',
-            'wait_for_transform' : '0.4',
             'imu_topic' : '/rtabmap/imu', # Placeholder path
             'gps_topic' : '/rtabmap/gps', # Placeholder path
+            'odom_topic' : '/odom',
+            'scan_topic' : '/scan', # LiDAR placeholder
+            
+            # Camera Arguments
+            'frame_id' : 'base_footprint',
+            'rgbd_sync' : 'true',
+	        'approx_rgbd_sync' : 'true',
+            'approx_sync_max_interval' : '0.01',
+            'subscribe_rgbd' : 'true',
+            'subscribe_depth' : 'false',
+            
+            # Odometry Arguments
+            'odom_frame_id' : 'odom',
+            'publish_tf_odom' : 'true',
+            'icp_odometry' : 'true', # Lidar Odom
+            'visual_odometry' : 'true', # Visual Odom
+            'imu_topic' : '/rtabmap/imu',
             'wait_imu_to_init' : 'false',
-            'map_topic' : '/map'
-            # More odometry arguments can be called
+            
+            # LiDAR Arguments
+            'subscribe_scan' : 'true',
+            
+            
+            
+            # Misc.
+            'log_level' : 'debug', 
+	        'approx_sync' : 'true',
+            'qos' : '1',
+	        'sync_queue_size' : '50',
+	        'topic_queue_size' : '50',  
+            'wait_for_transform' : '0.4',
+	        'rtabmap_viz' : 'true',
+            'map_topic' : '/map',
         }.items()
     )
     
