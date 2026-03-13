8d0c855be6a1d048d9b502e7f4a1df1d5210ce0a
rlj5 <lu.rosanna5@gmail.com>
2026-03-13 21:43:05 +0000

    merge conflicts

--- src/igvc_slam/launch/sim_rtabmap.launch.py
+++ src/igvc_slam/launch/sim_rtabmap.launch.py
@@ -40,9 +40,7 @@ def generate_launch_description():
             'wait_for_transform' : '0.4',
             'imu_topic' : '/rtabmap/imu',
             'wait_imu_to_init' : 'false',
-            'map_topic' : '/map'
-            
-
+            'map_topic' : '/map',
             'odom_tf_linear_variance': '0.001',
             'odom_tf_angular_variance': '0.001',
             'subscribe_rgbd': 'true',
@@ -51,10 +49,5 @@ def generate_launch_description():
             'scan_normal_k' : '0',   
             'scan_range_min' : '0.05',
             'scan_range_max' : '30.0',
-            'scan_cloud_max_points' : '2000',
+            'scan_cloud_max_points' : '2000'
         }.items()
-    )
-
-    return LaunchDescription([
-        rtabmap
-    ])
