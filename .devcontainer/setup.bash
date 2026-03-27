c0823a3c236d1a33e3cd01e458cb524668d2b5d3
tagciccone <tagciccone@gmail.com>
2026-03-27 22:30:24 +0000

    Basic refactor

--- .devcontainer/setup.bash
+++ .devcontainer/setup.bash
@@ -24,38 +24,40 @@ if [[ "$ENABLE_IMU" ]]; then
     mkdir -p build && cd build 
 
     cmake_options=""
-    if [ "$IMU_BACKEND" == "LOCAL" ]; then
-        cmake_options+="-DWITH_USB_BACKEND=OFF"
-        cmake_options+=" -DWITH_XML_BACKEND=OFF"
-        cmake_options+=" -DWITH_NETWORK_BACKEND=OFF"
-    fi
 
     if [ "$IMU_BACKEND" == "USB" ]; then 
         cmake_options+=" -DWITH_NETWORK_BACKEND=OFF"
-        cmake_options+=" -DWITH_LOCAL_BACKEND=OFF"
-        cmake_options+=" -DWITH_IIOD=OFF"
-    fi
-
-    if [ "$IMU_BACKEND" == "SERIAL" ]; then 
-        cmake_options+=" -DWITH_SERIAL_BACKEND=ON"
-        cmake_options+=" -DWITH_USB_BACKEND=OFF"
-        cmake_options+=" -DWITH_NETWORK_BACKEND=OFF"
-        cmake_options+=" -DWITH_LOCAL_BACKEND=OFF"
-        cmake_options+=" -DWITH_IIOD=OFF"
-    fi
-
-    if [ "$IMU_BACKEND" == "XML" ]; then 
-        cmake_options+=" -DWITH_NETWORK_BACKEND=OFF"
-        cmake_options+=" -DWITH_LOCAL_BACKEND=OFF"
-        cmake_options+=" -DWITH_USB_BACKEND=OFF"
-        cmake_options+=" -DWITH_IIOD=OFF"
+        cmake_options+=" -DWITH_LOCAL_BACKEND=OFF -DWITH_IIOD=OFF"
     fi
 
     if [ "$IMU_BACKEND" == "NETWORK" ]; then 
-        cmake_options+=" -DWITH_LOCAL_BACKEND=OFF"
         cmake_options+=" -DWITH_USB_BACKEND=OFF"
-        cmake_options+=" -DWITH_IIOD=OFF"
+        cmake_options+=" -DWITH_LOCAL_BACKEND=OFF -DWITH_IIOD=OFF"
     fi
+
+
+    if [ "$IMU_BACKEND" == "LOCAL" ]; then
+        cmake_options+=" -DWITH_USB_BACKEND=OFF"
+        cmake_options+=" -DWITH_NETWORK_BACKEND=OFF"
+        cmake_options+=" -DWITH_XML_BACKEND=OFF"
+    fi
+
+    if [ "$IMU_BACKEND" == "XML" ]; then 
+        cmake_options+=" -DWITH_USB_BACKEND=OFF"
+        cmake_options+=" -DWITH_NETWORK_BACKEND=OFF"
+        cmake_options+=" -DWITH_LOCAL_BACKEND=OFF -DWITH_IIOD=OFF"
+    fi
+
+
+    if [ "$IMU_BACKEND" == "SERIAL" ]; then 
+        cmake_options+=" -DWITH_SERIAL_BACKEND=ON"
+        
+        cmake_options+=" -DWITH_USB_BACKEND=OFF"
+        cmake_options+=" -DWITH_NETWORK_BACKEND=OFF"
+        cmake_options+=" -DWITH_LOCAL_BACKEND=OFF -DWITH_IIOD=OFF"
+    fi
+
+
 
     
 
