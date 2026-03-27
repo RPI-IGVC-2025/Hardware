422520ca1fa066b2ae9d5ee32c42faf2024e3c35
tagciccone <tagciccone@gmail.com>
2026-03-27 22:24:06 +0000

    Network backend

--- .devcontainer/setup.bash
+++ .devcontainer/setup.bash
@@ -31,13 +31,13 @@ if [[ "$ENABLE_IMU" ]]; then
     fi
 
     if [ "$IMU_BACKEND" == "USB" ]; then 
-        cmake_options+="-DWITH_NETWORK_BACKEND=OFF"
+        cmake_options+=" -DWITH_NETWORK_BACKEND=OFF"
         cmake_options+=" -DWITH_LOCAL_BACKEND=OFF"
         cmake_options+=" -DWITH_IIOD=OFF"
     fi
 
     if [ "$IMU_BACKEND" == "SERIAL" ]; then 
-        cmake_options+="-DWITH_SERIAL_BACKEND=ON"
+        cmake_options+=" -DWITH_SERIAL_BACKEND=ON"
         cmake_options+=" -DWITH_USB_BACKEND=OFF"
         cmake_options+=" -DWITH_NETWORK_BACKEND=OFF"
         cmake_options+=" -DWITH_LOCAL_BACKEND=OFF"
@@ -45,12 +45,20 @@ if [[ "$ENABLE_IMU" ]]; then
     fi
 
     if [ "$IMU_BACKEND" == "XML" ]; then 
-        cmake_options+="-DWITH_NETWORK_BACKEND=OFF"
+        cmake_options+=" -DWITH_NETWORK_BACKEND=OFF"
         cmake_options+=" -DWITH_LOCAL_BACKEND=OFF"
         cmake_options+=" -DWITH_USB_BACKEND=OFF"
         cmake_options+=" -DWITH_IIOD=OFF"
-
     fi
+
+    if [ "$IMU_BACKEND" == "NETWORK" ]; then 
+        cmake_options+=" -DWITH_LOCAL_BACKEND=OFF"
+        cmake_options+=" -DWITH_USB_BACKEND=OFF"
+        cmake_options+=" -DWITH_IIOD=OFF"
+    fi
+
+    
+
 
 
     echo "$cmake_options"
