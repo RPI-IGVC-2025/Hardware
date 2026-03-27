6e1b34dc12e34a48198951e6f6d6fe1b4cb4b07f
EnzoRap117 <enzorap117@gmail.com>
2026-03-27 21:36:40 +0000

    Commit because git won't let me pull develop

--- src/igvc_slam/launch/sim_rtabmap.launch.py
+++ src/igvc_slam/launch/sim_rtabmap.launch.py
@@ -53,7 +53,7 @@ def generate_launch_description():
 	        'sync_queue_size' : '50',
 	        'topic_queue_size' : '50',  
             'wait_for_transform' : '0.4',
-	        'rtabmap_viz' : 'true',
+	        #'rtabmap_viz' : 'true',
         }.items()
     )
     
@@ -65,83 +65,83 @@ def generate_launch_description():
         parameters=[os.path.join(package_slam, 'config/ekf.yaml'), {'use_sim_time': 'true'}]
     )    
     
-    # Stereo odometry
-    stereo_odom_node = Node(
-        package='rtabmap_odom', executable='stereo_odometry', name='stereo_odometry', output='screen',
-        emulate_tty=True,
-        parameters=[{
-            'frame_id' : 'frame_id',
-            'odom_frame_id' : 'vo_frame_id',
-            'publish_tf' : 'publish_tf_odom',
-            'ground_truth_frame_id' : 'ground_truth_frame_id',
-            'ground_truth_base_frame_id' : 'ground_truth_base_frame_id',
-            'wait_for_transform' : 'wait_for_transform',
-            'wait_imu_to_init' : 'wait_imu_to_init',
-            'always_check_imu_tf' : 'always_check_imu_tf',
-            'approx_sync' : 'approx_sync',
-            'approx_sync_max_interval' : 'approx_sync_max_interval',
-            'config_path' : 'cfg',
-            'topic_queue_size' : 'topic_queue_size',
-            'sync_queue_size' : 'sync_queue_size',
-            'qos' : 'qos_image',
-            'qos_camera_info' : 'qos_camera_info',
-            'qos_imu' : 'qos_imu',
-            'subscribe_rgbd' : 'subscribe_rgbd',
-            'guess_frame_id' : 'odom_guess_frame_id',
-            'guess_min_translation' : 'odom_guess_min_translation',
-            'guess_min_rotation' : 'odom_guess_min_rotation'}],
-        remappings=[{            
-            'left_image_topic_relay' : '/left/image_rect',
-            'right_image_topic_relay' : '/right/image_rect',
-            'left_camera_info_topic' : '/left/camera_info',
-            'right_camera_info_topic' : '/right/camera_info',
-            'rgbd_topic_relay' : '/rgbd_image',
-            'odom_topic' : '/odom_stereo',
-            'imu_topic' : '/imu'}],
-        arguments=['args', 'odom_args', "--ros-args", "--log-level", 'namespace', '.stereo_odometry:=', 'odom_log_level', "--log-level", 'stereo_odometry:=', 'odom_log_level'],
-        prefix= 'launch_prefix',
-        namespace= 'namespace'
-    ),
+    # # Stereo odometry
+    # stereo_odom_node = Node(
+    #     package='rtabmap_odom', executable='stereo_odometry', name='stereo_odometry', output='screen',
+    #     emulate_tty=True,
+    #     parameters=[{
+    #         'frame_id' : 'frame_id',
+    #         'odom_frame_id' : 'vo_frame_id',
+    #         'publish_tf' : 'publish_tf_odom',
+    #         'ground_truth_frame_id' : 'ground_truth_frame_id',
+    #         'ground_truth_base_frame_id' : 'ground_truth_base_frame_id',
+    #         'wait_for_transform' : 'wait_for_transform',
+    #         'wait_imu_to_init' : 'wait_imu_to_init',
+    #         'always_check_imu_tf' : 'always_check_imu_tf',
+    #         'approx_sync' : 'approx_sync',
+    #         'approx_sync_max_interval' : 'approx_sync_max_interval',
+    #         'config_path' : 'cfg',
+    #         'topic_queue_size' : 'topic_queue_size',
+    #         'sync_queue_size' : 'sync_queue_size',
+    #         'qos' : 'qos_image',
+    #         'qos_camera_info' : 'qos_camera_info',
+    #         'qos_imu' : 'qos_imu',
+    #         'subscribe_rgbd' : 'subscribe_rgbd',
+    #         'guess_frame_id' : 'odom_guess_frame_id',
+    #         'guess_min_translation' : 'odom_guess_min_translation',
+    #         'guess_min_rotation' : 'odom_guess_min_rotation'}],
+    #     remappings=[{            
+    #         'left_image_topic_relay' : '/left/image_rect',
+    #         'right_image_topic_relay' : '/right/image_rect',
+    #         'left_camera_info_topic' : '/left/camera_info',
+    #         'right_camera_info_topic' : '/right/camera_info',
+    #         'rgbd_topic_relay' : '/rgbd_image',
+    #         'odom_topic' : '/odom_stereo', # Hopefully changes where its publishing
+    #         'imu_topic' : '/imu'}],
+    #     arguments=['args', 'odom_args', "--ros-args", "--log-level", 'namespace', '.stereo_odometry:=', 'odom_log_level', "--log-level", 'stereo_odometry:=', 'odom_log_level'],
+    #     prefix= 'launch_prefix',
+    #     namespace= 'namespace'
+    # ),
 
-    # ICP odometry
-    icp_odom_node = Node(
-        package='rtabmap_odom', executable='icp_odometry', name="icp_odometry", output="screen",
-        name='icp_odometry_node',
-        emulate_tty=True,
-        parameters=[{
-            'frame_id' : 'frame_id',
-            'odom_frame_id' : 'vo_frame_id',
-            'publish_tf' : 'publish_tf_odom',
-            'ground_truth_frame_id' : 'ground_truth_frame_id',
-            'ground_truth_base_frame_id' : 'ground_truth_base_frame_id',
-            'wait_for_transform' : 'wait_for_transform',
-            'wait_imu_to_init' : 'wait_imu_to_init',
-            'always_check_imu_tf' : 'always_check_imu_tf',
-            'approx_sync' : 'approx_sync',
-            'approx_sync_max_interval' : 'approx_sync_max_interval',
-            'config_path' : 'cfg',
-            'topic_queue_size' : 'topic_queue_size',
-            'sync_queue_size' : 'sync_queue_size',
-            'qos' : 'qos_image',
-            'qos_camera_info' : 'qos_camera_info',
-            'qos_imu' : 'qos_imu',
-            'subscribe_rgbd' : 'subscribe_rgbd',
-            'guess_frame_id' : 'odom_guess_frame_id',
-            'guess_min_translation' : 'odom_guess_min_translation',
-            'guess_min_rotation' : 'odom_guess_min_rotation'}],
-        remappings=[{            
-            'scan_topic' : '/scan',
-            'scan_cloud_topic' : '/scan_cloud',
-            'odom_topic' : '/odom_icp',
-            'imu_topic' : '/imu'}],
-        arguments=['args', 'odom_args', "--ros-args", "--log-level", 'namespace', '.icp_odometry:=', 'odom_log_level'],
-        prefix= 'launch_prefix',
-        namespace= 'namespace'
-    ),   
+    # # ICP odometry
+    # icp_odom_node = Node(
+    #     package='rtabmap_odom', executable='icp_odometry', name="icp_odometry", output="screen",
+    #     name='icp_odometry_node',
+    #     emulate_tty=True,
+    #     parameters=[{
+    #         'frame_id' : 'frame_id',
+    #         'odom_frame_id' : 'vo_frame_id',
+    #         'publish_tf' : 'publish_tf_odom',
+    #         'ground_truth_frame_id' : 'ground_truth_frame_id',
+    #         'ground_truth_base_frame_id' : 'ground_truth_base_frame_id',
+    #         'wait_for_transform' : 'wait_for_transform',
+    #         'wait_imu_to_init' : 'wait_imu_to_init',
+    #         'always_check_imu_tf' : 'always_check_imu_tf',
+    #         'approx_sync' : 'approx_sync',
+    #         'approx_sync_max_interval' : 'approx_sync_max_interval',
+    #         'config_path' : 'cfg',
+    #         'topic_queue_size' : 'topic_queue_size',
+    #         'sync_queue_size' : 'sync_queue_size',
+    #         'qos' : 'qos_image',
+    #         'qos_camera_info' : 'qos_camera_info',
+    #         'qos_imu' : 'qos_imu',
+    #         'subscribe_rgbd' : 'subscribe_rgbd',
+    #         'guess_frame_id' : 'odom_guess_frame_id',
+    #         'guess_min_translation' : 'odom_guess_min_translation',
+    #         'guess_min_rotation' : 'odom_guess_min_rotation'}],
+    #     remappings=[{            
+    #         'scan_topic' : '/scan',
+    #         'scan_cloud_topic' : '/scan_cloud',
+    #         'odom_topic' : '/odom_icp', # Hopefully changes where its publishing
+    #         'imu_topic' : '/imu'}],
+    #     arguments=['args', 'odom_args', "--ros-args", "--log-level", 'namespace', '.icp_odometry:=', 'odom_log_level'],
+    #     prefix= 'launch_prefix',
+    #     namespace= 'namespace'
+    # ),   
 
     return LaunchDescription([
-        stereo_odom_node,
-        icp_odom_node,
+        #stereo_odom_node,
+        #icp_odom_node,
         rtabmap,
         robot_localization_node
     ])
