c155f320414c1ccac2e5ff0b3e2cace71b0044c9
Ashvin Ganesan <64051164+ashvinganesan@users.noreply.github.com>
2026-04-10 17:09:46 -0400

    added change so it is pulled through setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -14,6 +14,10 @@ if [ ! -d "imu_ros2" ]; then
     git clone https://github.com/analogdevicesinc/imu_ros2.git
 fi
 
+if [ ! -d "robot_self_filter" ]; then
+    git clone https://github.com/leggedrobotics/robot_self_filter.git
+fi
+
 if [ ! -d "libiio" ]; then
     git clone https://github.com/analogdevicesinc/libiio.git --branch v0.26
     cd libiio
