d753d5fc387cd674fe20b284c97037f2d255e770
pradeepg78 <pradg78@gmail.com>
2026-04-14 17:58:23 -0400

    added basic midpoint logic

--- src/igvc_goal/goal_setter.py
+++ src/igvc_goal/goal_setter.py
@@ -5,7 +5,7 @@ from rclpy.node import Node
 
 from nav_msgs.msg import Path
 from geometry_msgs.msg import PoseStamped
-from rclpy.action import ActionClient
+# from rclpy.action import ActionClient
 
 # this node publishes to /goal_update on PoseStamped, which is 
 # part of the geometry_msgs package. Nav2 subscribes to PoseStamped
@@ -40,14 +40,29 @@ class GoalSettingNode(Node):
         goal = PoseStamped()
         goal.header.stamp = self.get_clock().now().to_msg()
         goal.header.frame_id = 'base_footprint' # may have to change base_footprint. TODO look into 
-
-        # placeholder goal
-        goal.pose.position.x = 1.0
-        goal.pose.position.y = 0.0
+        
+        # get (x,y) coordinates for left and right boundaries
+        left_pt = self.left_boundary.poses[0].pose.position
+        right_pt = self.right_boundary.poses[0].pose.position
+        
+        # set the goal --> find the basic "halfway" point to determine path for now
+        goal.pose.position.x = (left_pt.x + right_pt.x) / 2.0
+        goal.pose.position.y = (left_pt.y + right_pt.y) / 2.0
         goal.pose.position.z = 0.0
         goal.pose.orientation.w = 1.0
 
+        # placeholder goal
+        # goal.pose.position.x = 1.0
+        # goal.pose.position.y = 0.0
+        # goal.pose.position.z = 0.0
+        # goal.pose.orientation.w = 1.0
+
         self.goal_pub.publish(goal)
+        self.get_logger().info(
+            f'Sent Goal: ({goal.pose.position.x}, {goal.pose.position.y})'
+        )
+        
+    
 
 
 def main(args=None):
