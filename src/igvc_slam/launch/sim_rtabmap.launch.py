0a3c352f9ac57cf74a380b46929fc8d02cbd5071
rlj5 <lu.rosanna5@gmail.com>
2026-04-15 19:21:19 -0400

    Feature/base link urdf (#105)
    * Drafted 2d_lidar for rtabmap
    
    * merge conflicts
    
    * Fixed typo and added RGBD/FromDepth parameters
    
    * Removed ODOM and edited parameters
    
    * Updating commit
    
    * Drafted 2d_lidar for rtabmap
    
    * merge conflicts
    
    * Fixed typo and added RGBD/FromDepth parameters
    
    * Removed ODOM and edited parameters
    
    * Draft of adding base_link
    
    * Edited joint
    
    * Changed joint placement
    
    * Moved base_footprint children to be children of base_link
    
    * Moved odom to be parent of base_link
    
    * Changed sensor files instead of creating new joints
    
    * Reverted lidar changes
    
    * Changed base_footprint to base_link in parameters
    
    * Changed frame id from base_footprint to base_link and changed /odom topic to odom

--- src/igvc_slam/launch/sim_rtabmap.launch.py
+++ src/igvc_slam/launch/sim_rtabmap.launch.py
@@ -21,10 +21,10 @@ def generate_launch_description():
             'rgb_topic' : '/camera/camera/color/image_raw',
             'camera_info_topic' : '/camera/camera/depth/camera_info',
             'approx_sync' : 'true',
-            'frame_id' : 'base_footprint',
+            'frame_id' : 'base_link',
             'log_level' : 'debug', 
             'publish_tf_odom' : 'true',
-            'odom_topic' : '/odom',
+            'odom_topic' : 'odom',
             'odom_frame_id' : 'odom',
             'sync_queue_size' : '10', #TODO verify
             'topic_queue_size' : '10', #TODO verify
