9b83124d1c91ff6b8b73fcb644df216d6dcd91b8
TagCiccone <tagciccone@gmail.com>
2026-05-27 00:12:32 +0000

    some experimenting with zed stuff

--- .devcontainer/setup.bash
+++ .devcontainer/setup.bash
@@ -17,6 +17,11 @@ if [ ! -d "ros_odrive" ] && $ENABLE_ODRIVE ; then
     git clone https://github.com/odriverobotics/ros_odrive.git
 fi
 
+if [ ! -d zed-ros2-wrapper ] && $HAS_ZED_DEPS; then 
+    git clone https://github.com/stereolabs/zed-ros2-wrapper.git
+fi
+
+
 if $ENABLE_IMU; then 
     if [ ! -d "imu_ros2" ]; then
         git clone https://github.com/analogdevicesinc/imu_ros2.git
