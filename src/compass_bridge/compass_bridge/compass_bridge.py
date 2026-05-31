a1de58153e91cc6e1720f65c7bbb3f0e8b307015
tagciccone <tagciccone@gmail.com>
2026-05-31 15:23:20 -0400

    Compass bridge driver

---
+++ src/compass_bridge/compass_bridge/compass_bridge.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from compass_bridge.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/compass_bridge/compass_bridge/compass_driver.py
@@ -0,0 +1,128 @@
+import smbus2
+import time
+import math
+import struct
+#X: -197.0, Y: 374.5, Z: 739.0
+# x_offset = -197.0
+# y_offset = 374.5
+# z_offset = 739.0
+
+##Scales  -> X: 0.7824858757062146, Y: 1.0146520146520146, Z: 1.3578431372549018
+# scale_x = 0.7824858757062146
+# scale_y = 1.0146520146520146
+# scale_z = 1.3578431372549018
+
+# Main Execution Loop
+JETSON_DEFAULT_I2C_BUS = 7
+DEFAULT_IST8310_ADDR = 0x0E
+
+CNTL_1 = 0x0A
+CNTL_2 = 0x0B
+
+
+class Compass:
+    def __init__(self, i2c_bus=JETSON_DEFAULT_I2C_BUS, address=DEFAULT_IST8310_ADDR, CONFIG_FILEPATH=None):
+        try:
+            # init compass
+            self._bus = smbus2.SMBus(i2c_bus)
+            self._address=address
+
+            self.soft_reset()
+            self.normal_mode()
+        except Exception as e:
+            print(f"Error initializing compass: {e}")
+            raise e
+
+        print(f"IST8310 compass on bus {i2c_bus} with address {address} initialized successfully.")
+
+        # get configs
+        import yaml
+
+        with open(CONFIG_FILEPATH, "r") as file:
+            try:
+                data = yaml.safe_load(file)
+                print(data)
+            except yaml.YAMLError as exception:
+                print(f"Error parsing YAML file: {exception}")
+
+        self.x_offset = data["offsets"]["x"]
+        self.y_offset = data["offsets"]["y"]
+        self.z_offset = data["offsets"]["z"]
+        self.scale_x = data["scales"]["x"]
+        self.scale_y = data["scales"]["y"]
+        self.scale_z = data["scales"]["z"]
+        self.declination_angle = data["declination_angle"]
+        
+
+
+    def soft_reset(self):
+        try:
+            self._bus.write_byte_data(self._address, CNTL_2, 0x01)
+            time.sleep(0.01)
+        except Exception as e:
+            print(f"Error performing soft reset: {e}")
+            raise e
+
+    def normal_mode(self):
+        try:
+            self._bus.write_byte_data(self._address, CNTL_1, 0x01)
+            time.sleep(0.01)
+        except Exception as e:
+            print(f"Error setting normal mode: {e}")
+            raise e
+        
+    def get_heading(self):
+        mx, my, mz = self.read_raw()
+
+        if mx is not None:
+            rad = math.atan2(my,mx)
+            deg = rad * 180.0 / math.pi
+            if deg < 0:
+                deg+=360
+            deg += self.declination_angle
+            if deg > 360:
+                deg-=360
+            elif deg < 0:
+                deg += 360
+            return deg
+        else:
+            return None
+
+
+
+    def read_raw(self):
+        try:
+            self._bus.write_byte_data(self._address, CNTL_1, 0x01)
+            while True:
+                status = self._bus.read_byte_data(self._address, 0x02)
+                if status & 0x01:
+                    break
+                time.sleep(0.005)
+
+            # Read 6 bytes (X_L, X_H, Y_L, Y_H, Z_L, Z_H)
+            data = self._bus.read_i2c_block_data(self._address, 0x03, 6)
+
+            # Convert to 16-bit signed integers (Little Endian)
+            x, y, z = struct.unpack('<hhh', bytes(data))
+
+            calibrated_x = (x - self.x_offset) * self.scale_x
+            calibrated_y = (y - self.y_offset) * self.scale_y
+            calibrated_z = (z - self.z_offset) * self.scale_z
+
+            tx = calibrated_y
+            ty = -calibrated_x
+            tz = calibrated_z
+
+            # Convert to uT (Microteslas) - scale is 0.3 uT/LSB according to IST8310 datasheet
+            scale = 0.3
+            return tx * scale, ty * scale,tz * scale
+        except Exception as e:
+            print(f"Error reading compass data: {e}")
+            raise e
+        
+    def cleanup(self):
+        try:
+            self._bus.close()
+        except Exception as e:
+            print(f"Error during cleanup: {e}")
+            raise e
