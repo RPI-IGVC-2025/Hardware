28a4003c3bb1798ad393e8ecf4ee83944924f73d
zbrotherton <zanebrotherton@gmail.com>
2026-05-31 16:21:17 +0000

    Rewrite cv setup

--- src/igvc_cv/setup.py
+++ src/igvc_cv/setup.py
@@ -1,27 +1,35 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
-package_name = 'igvc_cv'
+package_name = 'igvc_estop'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(),
+    packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join("share", package_name, "launch"), glob("launch/*.py")),
     ],
-    install_requires=['setuptools', 'numpy', 'opencv-python', 'sensor-msgs-py'],
+    install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='Camden Laursen-Carr',
-    maintainer_email='laursc@rpi.edu',
-    description='Computer Vision for the 2026 IGVC Robot',
-    license='MIT',
+    maintainer='root',
+    maintainer_email='root@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
     extras_require={
+        'test': [
+            'pytest',
+        ],
     },
     entry_points={
-    'console_scripts': [
-        'lane_points_node = igvc_cv.lane_points_node:main',
-    ],
-},
+        'console_scripts': [
+            "lane_points_node = igvc_cv.lane_points_node:main",
+        ],
+    },
 )
+
+
