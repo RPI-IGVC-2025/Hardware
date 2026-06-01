81de8db0701d50dafcd762033b6b20ad5c20e230
zbrotherton <zanebrotherton@gmail.com>
2026-06-01 10:37:59 +0000

    change imu name navsat

--- src/igvc_slam/launch/dual_ekf.launch.py
+++ src/igvc_slam/launch/dual_ekf.launch.py
@@ -34,7 +34,7 @@ def generate_launch_description():
         output='screen',
         parameters=[param_file],
         remappings=[
-            ('imu/data', '/compass'), #TODO: change to compass imu
+            ('imu', '/compass'), #TODO: change to compass imu
             ('gps/fix', '/fix'),
             ('gps/filtered', '/gps/filtered'),
             ('odometry/gps', '/odometry/gps'),
