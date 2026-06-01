d4c04988a435523ed4dff8c0280e7e41ff0d3cc9
zbrotherton <zanebrotherton@gmail.com>
2026-06-01 10:24:28 +0000

    change to compass topic

--- src/igvc_slam/launch/dual_ekf.launch.py
+++ src/igvc_slam/launch/dual_ekf.launch.py
@@ -34,7 +34,7 @@ def generate_launch_description():
         output='screen',
         parameters=[param_file],
         remappings=[
-            ('imu/data', '/zed/zed_node/imu/data'), #TODO: change to compass imu
+            ('imu/data', '/compass'), #TODO: change to compass imu
             ('gps/fix', '/fix'),
             ('gps/filtered', '/gps/filtered'),
             ('odometry/gps', '/odometry/gps'),
